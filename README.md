0ed28b825af8977b43c9c9e5c2464edf64f6831d
Unknown <tristan.legranche@gmail.com>
2019-03-08 17:47:57 +0100

    [MAJOR] add an UI
    And correct some bugs

--- README.md
+++ README.md
@@ -1,10 +1,101 @@
 # UV Snapshot Exporter
 ## version 0.1
 
-This tool is used to export the uvSnapshots of all the selected meshes
+This tool is used to export the uvSnapshots of all the uvSets of all selected meshes
 
+## Installation
 
+* Put the UVSnapshotExporter folder in a maya script directory (I.e: C:\Users\user\Documents\maya\scripts on Windows)    
+![install_path](pics/01_scriptPath.jpg)
 
+* The content of the folder need to be this one  
+![Folder content](pics/02_scriptContent.jpg)  
+ 
+* Create a shelf button in Maya with the following Python command  
+![Shelf command](pics/03_shelfContent.jpg)  
+
+## How to use
+1 - Open a scene
+
+![Open scene](pics/04_openScene.jpg)  
+
+2 - Click on the shelf button to launch the tool  
+
+![Launch the tool](pics/10_launchScript.jpg)  
+
+3 - Select objects  
+
+![Launch the tool](pics/11_selectObjects.jpg)
+
+4 - Define the parameters
+
+5 - Click on proceed  
+
+![Launch](pics/12_proceed.jpg)  
+
+6 - Enjoy !  
+
+![Enjoy](pics/14_UVs.jpg)  
+
+## Features details
+
+* #### Options
+Use the **Get** button to set the folder where the snapshots will be created (default: project/sourceImages)
+
+Use UV Folder to create a subdirectory in the specified path (leave empty if you don't need a subdirectory)
+
+Use UV suffix to add a suffix to the maps name (don't forget to add a '_' or a '-'... otherwise the name will be stacked)
+
+Use File type to specify the extension of the snapshots
+
+Use Map resolution to specify the resolution of the snapshots
+
+Use Color to specify the color of the UVs in the snapshots
+
+* #### Proceed
+
+This button launch the snapshot export
+
+## Limitations
+
+1. Only work on selected objects, the script doesn't look in the hierarchy
+2. Can be a bit slow if you export a lot of UVs at once
+
+## Step by step on a case
+1 - A scene is opened  
+
+![Open scene](pics/04_openScene.jpg)
+    
+2 - The objects have multiple uvSets  
+
+![Open scene](pics/05_uvSets.jpg)  
+
+3 - Like this one  
+
+![Open scene](pics/06_uvSet01.jpg)  
+
+![Open scene](pics/07_uvSet02.jpg)  
+
+4 - I select the objects to export there UVSnapshots  
+
+![Open scene](pics/11_selectObjects.jpg)    
+
+5 - I launch the script and set the options to export in project/sourceImages, create a new UV folder in it, have a _UVSnap prefix and make 2048x2048 png files with green UVs  
+
+![Open scene](pics/10_launchScript.jpg)  
+
+6 - I click on proceed  
+
+![Open scene](pics/12_proceed.jpg)  
+
+7 - Here it is, I've got a UV folder in project/sourceImages  
+
+![Open scene](pics/13_textureFolderContent.jpg)  
+
+8 - With UVSnaphots in it  
+
+![Open scene](pics/14_UVs.jpg)        
+  
 
 ## Credits
 
--- UVSnapshotExporter/uvSnapshotExporter.py
+++ UVSnapshotExporter/uvSnapshotExporter.py
@@ -10,38 +10,260 @@
 #
 ################################
 
-uvPath = 'UV'            # The folder in sourceImage used to stock the uv snapshots
-uvColor = 'green'        # Choose between red, green and blue
-uvFileType = 'png'       # The file type you want to use for the snapshots
-uvSize = 2048            # The pixel resolution of the snapshots
-uvSuffixe = 'uvSnap'    # The name of the snapshots will be mesh_uvSet_uvSuffixe
-
+# Libraries
+import os
 import maya.cmds as mc
+from PySide2 import QtCore
+from PySide2 import QtWidgets
 
-meshes = mc.ls(selection=True)
-actualWorkspace = mc.workspace(fullName=True)
+# Variables
+PLUGIN_NAME = 'UV Snapshot exporter'
+PLUGIN_VERSION = '0.1'
+TEXTURE_FOLDER = 'sourceImages'
+INFOS = 'Report bugs to tristan.legranche@gmail.com \n\nTool under licence CC-BY-NC \nContact me for commercial use'
+FILE_TYPES = ['png', 'jpg', 'iff', 'tga']
+SIZE = ['4096', '2048', '1024', '512', '256', '128']
+COLORS = ['red', 'green', 'blue', 'pink', 'yellow', 'white', 'black']
 
-for mesh in meshes:
-    
-    for shape in mc.listRelatives(mesh, children=True, shapes=True):
-        if mc.objectType(shape)=='mesh': 
-            mc.select(shape, replace=True)   
-            uvSets = mc.polyUVSet(allUVSets=True, query=True)
-            
-            for uvSet in uvSets:
-                if uvColor == 'green':
-                    red = blue = 0
-                    green = 255
-                elif uvColor == 'blue':
-                    red = green = 0
-                    blue = 255
-                elif uvColor == 'red':
-                    green = blue = 0
-                    red = 255
-                
-                fileName = actualWorkspace + '/sourceImages/' + mesh + '_' + uvSet + '_' + uvSuffixe + '.' + uvFileType
-                mc.uvSnapshot(overwrite=True, fileFormat=uvFileType, xResolution=uvSize, yResolution=uvSize, antiAliased=True, redColor=red, greenColor=green, blueColor=blue, name=fileName)
-                print fileName + ' as been created'
-                
-            
-mc.select(meshes, replace=True)
+
+class Exporter:
+
+    def __init__(self):
+
+        print('\n\n' + PLUGIN_NAME + ' version ' + PLUGIN_VERSION + '\n')
+        self.actualWorkspace = mc.workspace(fullName=True)
+        self.initUI()
+
+    def initUI(self):
+        """
+        Creates the UI
+        :return: None
+        """
+
+        # Create our main window
+        self.mainWindow = QtWidgets.QDialog()
+        self.mainWindow.setWindowTitle(PLUGIN_NAME + ' version ' + PLUGIN_VERSION)
+        self.mainWindow.setFixedSize(350, 350)
+        self.mainWindow.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+
+        # Create vertical layout
+        self.layVMainWindowMain = QtWidgets.QVBoxLayout()
+        self.mainWindow.setLayout(self.layVMainWindowMain)
+        self.mainWindow.setStyleSheet("""
+                                       QGroupBox {  }
+                                       """)
+
+        # Texture Folder
+        self.grpOptions = QtWidgets.QGroupBox('Options')
+        self.layVMainWindowMain.addWidget(self.grpOptions)
+
+        self.optionsLayout = QtWidgets.QVBoxLayout()
+        self.grpOptions.setLayout(self.optionsLayout)
+
+        # Create the layout
+        self.optionsSubLayout4 = QtWidgets.QHBoxLayout()
+        self.optionsLayout.insertLayout(0, self.optionsSubLayout4, stretch=0)
+
+        # Create the widgets
+        sourceImagesFolder = self.actualWorkspace + '/' + TEXTURE_FOLDER
+        self.texturePath = QtWidgets.QLineEdit(sourceImagesFolder)
+        self.optionsSubLayout4.addWidget(self.texturePath)
+
+        self.getButton = QtWidgets.QPushButton('Get')
+        self.getButton.clicked.connect(lambda: self.getTextureFolder())
+        self.optionsSubLayout4.addWidget(self.getButton)
+
+
+        # Create the layout
+        self.optionsSubLayout3 = QtWidgets.QHBoxLayout()
+        self.optionsLayout.insertLayout(1, self.optionsSubLayout3, stretch=0)
+
+        # Create the widgets
+        self.map1 = QtWidgets.QLabel('UV folder')
+        self.optionsSubLayout3.addWidget(self.map1)
+
+        self.uvFolder = QtWidgets.QLineEdit('UV')
+        self.optionsSubLayout3.addWidget(self.uvFolder)
+
+
+        # Create the layout
+        self.optionsSubLayout5 = QtWidgets.QHBoxLayout()
+        self.optionsLayout.insertLayout(2, self.optionsSubLayout5, stretch=0)
+
+        # Create the widgets
+        self.map1 = QtWidgets.QLabel('UV suffix')
+        self.optionsSubLayout5.addWidget(self.map1)
+
+        self.uvSuffix = QtWidgets.QLineEdit('_UVSnap')
+        self.optionsSubLayout5.addWidget(self.uvSuffix)
+
+
+        # Create the layout
+        self.optionsSubLayout1 = QtWidgets.QHBoxLayout()
+        self.optionsLayout.insertLayout(3, self.optionsSubLayout1, stretch=0)
+
+        # Create the widgets
+        self.map1 = QtWidgets.QLabel('File type')
+        self.optionsSubLayout1.addWidget(self.map1)
+
+        self.fileExtension = QtWidgets.QComboBox()
+        self.fileExtension.addItems(FILE_TYPES)
+        self.fileExtension.setCurrentIndex(0)
+        self.optionsSubLayout1.addWidget(self.fileExtension)
+
+
+        # Create the layout
+        self.optionsSubLayout2 = QtWidgets.QHBoxLayout()
+        self.optionsLayout.insertLayout(4, self.optionsSubLayout2, stretch=0)
+
+        # Create the widgets
+        self.map1 = QtWidgets.QLabel('Map resolution')
+        self.optionsSubLayout2.addWidget(self.map1)
+
+        self.resolution = QtWidgets.QComboBox()
+        self.resolution.addItems(SIZE)
+        self.resolution.setCurrentIndex(1)
+        self.optionsSubLayout2.addWidget(self.resolution)
+
+
+        # Create the layout
+        self.optionsSubLayout6 = QtWidgets.QHBoxLayout()
+        self.optionsLayout.insertLayout(5, self.optionsSubLayout6, stretch=0)
+
+        # Create the widgets
+        self.map1 = QtWidgets.QLabel('Color')
+        self.optionsSubLayout6.addWidget(self.map1)
+
+        self.color = QtWidgets.QComboBox()
+        self.color.addItems(COLORS)
+        self.color.setCurrentIndex(1)
+        self.optionsSubLayout6.addWidget(self.color)
+
+        # Create the layout
+        self.optionsSubLayout7 = QtWidgets.QHBoxLayout()
+        self.optionsLayout.insertLayout(6, self.optionsSubLayout7, stretch=0)
+
+        # Create the widgets
+        self.proceedButton = QtWidgets.QPushButton('Proceed')
+        self.proceedButton.clicked.connect(lambda: self.main())
+        self.optionsSubLayout7.addWidget(self.proceedButton)
+
+        # Infos
+        self.grpInfos = QtWidgets.QGroupBox('Credits')
+        self.layVMainWindowMain.addWidget(self.grpInfos)
+
+        self.infosLayout = QtWidgets.QVBoxLayout()
+        self.grpInfos.setLayout(self.infosLayout)
+
+        # Infos widgets
+        self.infos = QtWidgets.QLabel(INFOS)
+        self.infosLayout.addWidget(self.infos)
+        self.infos.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
+
+        global window
+
+        try:
+            window.close()
+            window.deleteLater()
+        except:
+            pass
+
+        window = self.mainWindow
+        self.mainWindow.show()
+        print('UI opened')
+
+    def getTextureFolder(self):
+        """
+        Get the base texture path in the interface, the file dialog start in the base texture path of the project
+        :return: The texture directory
+        """
+
+        # Get project
+        projectDirectory = mc.workspace(rootDirectory=True, query=True)
+
+        # Set base texture folder
+        textureFolder = projectDirectory + '/' + TEXTURE_FOLDER
+
+        if os.path.isdir(textureFolder):
+            sourceImages = textureFolder
+        else:
+            sourceImages = projectDirectory
+
+        # Open a file dialog
+        workDirectory = mc.fileDialog2(startingDirectory=sourceImages, fileMode=2, okCaption='Select')[0]
+
+        # Update the texture path in the interface
+        self.texturePath.setText(workDirectory)
+
+        return workDirectory
+
+    def main(self):
+
+        texturesPath = self.texturePath.text()
+        uvPath = self.uvFolder.text()
+        uvSuffix = self.uvSuffix.text()
+        fileExtension = self.fileExtension.currentText()
+        mapResolution = self.resolution.currentText()
+        color = self.color.currentText()
+
+        meshes = mc.ls(selection=True)
+
+        print('EXPORT STARTED')
+
+        for mesh in meshes:
+
+            shapes = mc.listRelatives(mesh, children=True, shapes=True)
+
+            if shapes:
+
+                for shape in shapes:
+                    if mc.objectType(shape)=='mesh':
+
+                        mc.select(shape, replace=True)
+
+                        uvSets = mc.polyUVSet(allUVSets=True, query=True)
+
+                        for uvSet in uvSets:
+
+                            if color == 'green':
+                                red = blue = 0
+                                green = 255
+
+                            elif color == 'blue':
+                                red = green = 0
+                                blue = 255
+
+                            elif color == 'red':
+                                green = blue = 0
+                                red = 255
+
+                            elif color == 'pink':
+                                red = blue = 255
+                                green = 0
+                            elif color == 'yellow':
+                                red = green = 255
+                                blue = 0
+
+                            elif color == 'white':
+                                red = green = blue = 255
+
+                            elif color == 'black':
+                                red = green = blue = 0
+
+                            fileName = texturesPath + '/' + uvPath
+                            if not fileName == texturesPath:
+                                fileName += '/'
+
+                            if not os.path.exists(fileName):
+                                os.makedirs(fileName)
+
+                            fileName += mesh + '_' + uvSet + uvSuffix + '.' + fileExtension
+
+                            mc.polyUVSet(currentUVSet=True, uvSet=uvSet)
+
+                            mc.uvSnapshot(overwrite=True, fileFormat=fileExtension, xResolution=int(mapResolution), yResolution=int(mapResolution), antiAliased=True, redColor=red, greenColor=green, blueColor=blue, name=fileName)
+                            print fileName + ' as been created'
+
+        mc.select(meshes, replace=True)
+
+        print('EXPORT FINISHED')
